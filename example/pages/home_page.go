283456be239a97fb8a3c56ebebe860e5619464a5
bodhi <bodhi@5263.org>
2016-10-21 23:52:27 +0800

    Exmaple container combinator

--- example/pages/home_page.go
+++ example/pages/home_page.go
@@ -17,10 +17,17 @@ func makeContainer(label int) containers.Container {
 	}
 }
 
-func (hp *HomePage) Containers(r *http.Request, ctx context.Context) (cs []containers.Container, err error) {
+func repeat(c containers.Container) containers.Container {
+	return func(r *http.Request, ctx context.Context) (html string, err error) {
+		out, _ := c(r, ctx)
+		out2, _ := c(r, ctx)
+		return out + out2, nil
+	}
+}
 
+func (hp *HomePage) Containers(r *http.Request, ctx context.Context) (cs []containers.Container, err error) {
 	return []containers.Container{
-		makeContainer(1),
+		repeat(makeContainer(1)),
 		makeContainer(2),
 		makeContainer(3),
 	}, nil
