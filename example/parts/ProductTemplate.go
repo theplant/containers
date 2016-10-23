53c458e44be348ab41b285c0d0c9aafcf8c092ec
bodhi <bodhi@5263.org>
2016-10-23 22:58:42 +0800

    Update addtocart template to call function directly
    Avoid listening to all click events on document

--- example/parts/ProductTemplate.go
+++ example/parts/ProductTemplate.go
@@ -10,7 +10,7 @@ func ProductTemplate(p *models.Product) string {
 	var _buffer bytes.Buffer
 	_buffer.WriteString("\n<div>\n    <h1>")
 	_buffer.WriteString(gorazor.HTMLEscape(p.Name))
-	_buffer.WriteString("</h1>\n\n    <form>\n        <input type=\"hidden\" name=\"VariantId\" value=\"111222\"/>\n        <a href=\"#\" data-container-event=\"cart_updated\" class=\"addToCart\">Add To Cart</a>\n    </form> \n\n</div>\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/fetch/1.0.0/fetch.min.js\"></script>\n<script type=\"text/javascript\">\n\ndocument.addEventListener(\"click\", postAction);\n\nfunction postAction(e) {\n    console.log(e)\n    const event = e.target.dataset.containerEvent\n    if (event != null) {\n        var varid = document.querySelector(\"input[name=VariantId]\")\n        fetch(\"/actions/addToCart\", {\n            VariantId: varid.value\n        }).then(function(res){\n            postEvent(event)\n        })\n    }\n}\n\n</script>")
+	_buffer.WriteString("</h1>\n    <button onclick=\"addToCart(111222)\" class=\"addToCart\">Add To Cart</button>\n\n</div>\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/fetch/1.0.0/fetch.min.js\"></script>\n<script type=\"text/javascript\">\n\nfunction addToCart(variantId) {\n    const body = new FormData();\n    body.append(\"VariantId\", variantId);\n    fetch(\"/actions/addToCart\", {\n        method: \"POST\",\n        body: body,\n    }).then(function(res){\n        postEvent(\"cart_updated\")\n    }).catch(e => alert(e));\n}\n\n</script>")
 
 	return _buffer.String()
 }
